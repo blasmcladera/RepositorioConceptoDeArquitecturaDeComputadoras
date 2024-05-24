36dd27b9b61aeb32069e0ea173f749fdc2177433
blasmcladera <166237941+blasmcladera@users.noreply.github.com>
2024-05-24 15:36:24 -0300

    Update e7.s

--- Practica 4/e7.s
+++ Practica 4/e7.s
@@ -5,16 +5,19 @@ TABLA:	.word 0,1,2,3,4,5,6,7,8,9
 RES:	.word 0,0,0,0,0,0,0,0,0,0
 
 	.code
-	lw $t0,X($zero)
-	daddi $t0,$t0,1
-	lw $t1,CANT($zero)
-	daddi $t2,$zero,0
-FOR:	lw $t3,TABLA($t2)
+	dadd $t2,$zero,$zero
+	ld $t0,X($zero)
+	ld $t1,CANT($zero)
+	daddi $t6,$zero,80
+FOR:	ld $t3,TABLA($t2)
+	nop
 	slt $t4,$t3,$t0
 	xori $t4,$t4,1
+	beq $t3,$t0,CONT
 	dadd $t1,$t1,$t4
 	sw $t4,RES($t2)
-	daddi $t5,$t2,-10
-	beqz $t5,FOR
+CONT:	daddi $t2,$t2,8
+	nop
+	bne $t2,$t6,FOR
 	sw $t1,CANT($zero)
 	halt
