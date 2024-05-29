9430192ddf0e86899e54a3364961e617e59751aa
blasmcladera <166237941+blasmcladera@users.noreply.github.com>
2024-05-29 15:39:52 -0300

    e10

--- Practica 4/e3.s
+++ Practica 4/e3.s
@@ -1,12 +1,13 @@
-.data
-A: .word 1
-B: .word 6
-C: .word 0
-.code
-ld r1, A(r0)
-ld r2, B(r0)
-loop: dsll r1, r1, 1
-daddi r2, r2, -1
-bnez r2, loop
-sd r1,C(r0)
-halt
+	.data
+base:	.double 5.85
+altura:	.double	13.47
+result:	.double 2
+
+	.code
+	l.d f0,base($zero)
+	l.d f1,altura($zero)
+	mul.d f0,f0,f1
+	l.d f2,result($zero)
+	div.d f0,f0,f2
+	s.d f0,result($zero)
+	halt
